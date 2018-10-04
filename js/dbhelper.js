3efb8d00104de392da0b8100deea005423ae78ac
Melissa Key West <art@mkaydub.com>
2018-10-03 21:11:44 -0600

    Fix for Error on display of images - githubpages

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -150,21 +150,21 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+    return (`./img/${restaurant.photograph}`);
   }
 
   /**
    * Index image Srcset.
    */
   static imageSrcsetForIndex(restaurant) {
-    return (`/img/${restaurant.id}-300.jpg 1x, /img/${restaurant.id}-600_2x.jpg 2x`);
+    return (`./img/${restaurant.id}-300.jpg 1x, /img/${restaurant.id}-600_2x.jpg 2x`);
   }
 
   /**
    * Restaurant image Srcset.
    */
   static imageSrcsetForRestaurant(restaurant) {
-    return (`/img/${restaurant.id}-300.jpg 300w, /img/${restaurant.id}-400.jpg 400w, /img/${restaurant.id}-600_2x.jpg 600w, /img/${restaurant.id}-800_2x.jpg 800w`);
+    return (`./img/${restaurant.id}-300.jpg 300w, /img/${restaurant.id}-400.jpg 400w, /img/${restaurant.id}-600_2x.jpg 600w, /img/${restaurant.id}-800_2x.jpg 800w`);
   }
 
 
